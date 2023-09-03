aaec048494153d018a3a15d4d3a4b0c7868e78e9
Holland Ninow <holland.ninow@gmail.com>
2023-09-03 10:48:09 -0700

    add company routes

---
+++ routers/companyRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const companyController = require('../controllers/companyController');
+
+const router = express.Router();
+
+// TODO: add protect() and restrictTo() middlewares after implementing authController
+
+router
+  .route('/')
+  .get(companyController.getAllCompanies)
+  .post(companyController.createWord);
+
+router
+  .route('/:id')
+  .get(companyController.getCompany)
+  .patch(companyController.updateCompany)
+  .delete(companyController.deleteCompany);
+
+module.exports = router;
