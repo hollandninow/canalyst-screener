3a5390957e4e0f0dd46cbdb685f9be4d96936105
Holland Ninow <holland.ninow@gmail.com>
2023-09-06 08:57:20 -0700

    add createPasswordResetToken schema method

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -91,6 +90,19 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
   return false;
 }
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
 
 const User = mongoose.model('User', userSchema);
 
