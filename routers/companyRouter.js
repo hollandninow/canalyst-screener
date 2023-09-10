56a0b54dc43dbcbcb31b0308a4d034b00e2abaae
Holland Ninow <holland.ninow@gmail.com>
2023-09-10 11:53:40 -0700

    fix typo

--- routers/companyRouter.js
+++ routers/companyRouter.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router
   .route('/')
   .get(companyController.getAllCompanies)
-  .post(companyController.createWord);
+  .post(companyController.createCompany);
 
 router
   .route('/:id')
