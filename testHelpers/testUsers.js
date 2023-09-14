06cfaed51dcd71ba42cee687c2c5c3d0e1126207
Holland Ninow <holland.ninow@gmail.com>
2023-09-14 08:49:00 -0700

    test users object collection for testing

---
+++ testHelpers/testUsers.js
@@ -0,0 +1,52 @@
+const dotenv = require('dotenv');
+dotenv.config( { path: './config.env' });
+
+exports.adminTestUser = {
+  email: process.env.ADMIN_TEST,
+  password: process.env.ADMIN_TEST_PASSWORD,
+}
+
+exports.testUser = {
+  name: 'Test',
+  email: 'test@gmail.com',
+  password: 'test1234',
+  passwordConfirm: 'test1234',
+}
+
+exports.testUserNoName = {
+  email: 'test@gmail.com',
+  password: 'test1234',
+  passwordConfirm: 'test1234',
+}
+
+exports.testNoEmail = {
+  name: 'Test',
+  password: 'test1234',
+  passwordConfirm: 'test1234',
+}
+
+exports.testUserMalformedEmail = {
+  name: 'Test',
+  email: 'bademail',
+  password: 'test1234',
+  passwordConfirm: 'test1234',
+}
+
+exports.testUserNoPassword = {
+  name: 'Test',
+  email: 'test@gmail.com',
+  passwordConfirm: 'test1234',
+}
+
+exports.testUserNoPasswordConfirm = {
+  name: 'Test',
+  email: 'test@gmail.com',
+  password: 'test1234',
+}
+
+exports.testUserMismatchPasswords = {
+  name: 'Test',
+  email: 'test@gmail.com',
+  password: 'test12345',
+  passwordConfirm: 'test1234',
+}
