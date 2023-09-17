67abeaa97ea74b954ca6fb7a43dc96e66e71b8da
Holland Ninow <holland.ninow@gmail.com>
2023-09-16 18:50:19 -0700

    add well formed test company object for testing

---
+++ testHelpers/testCompanies.js
@@ -0,0 +1,16 @@
+exports.testCompany = {
+  name: 'The Test Company',
+  csin: '1Q2W3E4R5T',
+  canalystTicker: 'TEST_US',
+  bloombergTicker: 'TEST US',
+  countryCode: 'us',
+  version: 'Q3-2023.25',
+  mostRecentPeriod: 'Q3-2023',
+  mrpDuration: 'fiscal_quarter',
+  mrpStartDate: '2023-04-01',
+  mrpEndDate: '2023-06-30',
+  isInCoverage: true,
+  reportingFrequency: 'QFY',
+  tradingCurrency: 'USD',
+  reportingCurrency: 'USD',
+}
