3e438eadd62231d4a356be663aff518f3ab5db70
Holland Ninow <holland.ninow@gmail.com>
2023-09-25 19:17:26 -0700

    add first company test case

--- test/companies.js
+++ test/companies.js
@@ -2,6 +2,7 @@ const supertest = require('supertest');
 const { expect } = require('chai');
 const dotenv = require('dotenv');
 const testUsers = require('../testHelpers/testUsers');
+const testCompanies = require('../testHelpers/testCompanies');
 
 dotenv.config({ path: './config.env' });
 
@@ -20,7 +21,44 @@ describe('companies', () => {
   );
 
   describe('POST companies', () => {
+    afterEach(() => 
+      request
+        .delete(`api/v1/companies/${testCompanyId}`)
+        .set('Authorization', `Bearer ${adminToken}`)
+        .then(res => {
+          testCompanyId = undefined;
+        })
+    );
 
+    it('should create a new company successfully when provided all required data', done => {
+      request
+        .post('api/v1/companies/')
+        .set('Authorization', `Bearer ${adminToken}`)
+        .send(testCompanies.testCompany)
+        .expect(201)
+        .then(res => {
+          const { data } = res.body.data;
+          testCompanyId = data._id;
+
+          expect(data.name).to.be.equal(testCompanies.testCompany.name);
+          expect(data.csin).to.be.equal(testCompanies.testCompany.csin);
+          expect(data.canalystTicker).to.be.equal(testCompanies.testCompany.canalystTicker);
+          expect(data.bloombergTicker).to.be.equal(testCompanies.testCompany.bloombergTicker);
+          expect(data.countryCode).to.be.equal(testCompanies.testCompany.countryCode);
+          expect(data.version).to.be.equal(testCompanies.testCompany.version);
+          expect(data.mostRecentPeriod).to.be.equal(testCompanies.testCompany.mostRecentPeriod);
+          expect(data.mrpDuration).to.be.equal(testCompanies.testCompany.mrpDuration);
+          expect(data.mrpStartDate).to.be.equal(testCompanies.testCompany.mrpStartDate);
+          expect(data.mrpEndDate).to.be.equal(testCompanies.testCompany.mrpEndDate);
+          expect(data.isInCoverage).to.be.equal(testCompanies.testCompany.isInCoverage);
+          expect(data.reportingFrequency).to.be.equal(testCompanies.testCompany.reportingFrequency);
+          expect(data.tradingCurrency).to.be.equal(testCompanies.testCompany.tradingCurrency);
+          expect(data.reportingCurrency).to.be.equal(testCompanies.testCompany.reportingCurrency);
+
+          done();
+        })
+        .catch(err => done(err));
+    })
   });
   
   describe('GET companies', () => {
