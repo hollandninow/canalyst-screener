f695f54bd4803ee9a8d4d636a361c5bb93b6790b
Holland Ninow <holland.ninow@gmail.com>
2023-10-04 18:34:27 -0700

    add fake id #

--- testHelpers/testCompanies.js
+++ testHelpers/testCompanies.js
@@ -1,10 +1,28 @@
 exports.permanentTestCompanyId = '6505e90c570f2ede901a970e';
+exports.fakeTestCompanyId = '6505e90c57819812313iuhn0e';
 
 exports.testCompany = {
   name: 'The Test Company',
   csin: '1Q2W3E4R5T',
   canalystTicker: 'TEST_US',
   bloombergTicker: 'TEST US',
+  countryCode: 'us',
+  version: 'Q3-2023.25',
+  mostRecentPeriod: 'Q3-2023',
+  mrpDuration: 'fiscal_quarter',
+  mrpStartDate: '2023-04-01',
+  mrpEndDate: '2023-06-30',
+  isInCoverage: true,
+  reportingFrequency: 'QFY',
+  tradingCurrency: 'USD',
+  reportingCurrency: 'USD',
+}
+
+exports.permanentTestCompany = {
+  name: 'The Permanent Test Company',
+  csin: 'Q1W2E3R4T5',
+  canalystTicker: 'PERM_US',
+  bloombergTicker: 'PERM US',
   countryCode: 'us',
   version: 'Q3-2023.25',
   mostRecentPeriod: 'Q3-2023',
