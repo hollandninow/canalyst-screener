9ea5af274610aa54554ec7ac29acbe9ecb9b6380
Holland Ninow <holland.ninow@gmail.com>
2023-11-17 08:48:06 -0800

    new methods for initiating company list and getting company list

--- MDSNavigator/MDSNavigator.js
+++ MDSNavigator/MDSNavigator.js
@@ -1,16 +1,27 @@
 const axios = require('axios');
+const MDSCompanyList = require('./MDSCompanyList');
 
 class MDSNavigator {
   APIRootURL = 'https://mds.canalyst.com/api/';
 
   constructor(token) {
+    this.token = token;
+
     this.instance = axios.create({
       baseURL: this.APIRootURL,
       timeout: 20000,
       headers: {
-        Authorization: 'Bearer ' + token,
+        Authorization: 'Bearer ' + this.token,
       }
     });
+  }
+
+  async initCompanyList(options) {
+    this.companyListArray = await new MDSCompanyList(this.token).getCompanyList(options);
+  }
+
+  getCompanyListArray() {
+    return this.companyListArray;
   }
 }
 
