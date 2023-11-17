1780429252071726ec50d507c7c15c6d2834db3f
Holland Ninow <holland.ninow@gmail.com>
2023-11-17 08:14:10 -0800

    fixed bug

--- utils/convertCSVToJSON.js
+++ utils/convertCSVToJSON.js
@@ -1,3 +1,3 @@
 const Papa = require('papaparse');
 
-exports.convertCSVToJSON = csv => Papa.parse(csv, { header: true }).data;
+exports.convertCSVToJSON = csv => Papa.parse(csv, { header: true }).data.slice(0,-1);
