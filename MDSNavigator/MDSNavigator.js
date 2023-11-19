453dfcb0197e29ec8f6f88a2b95d652941b1a786
Holland Ninow <holland.ninow@gmail.com>
2023-11-19 10:40:58 -0800

    add error handling to constructor

--- MDSNavigator/MDSNavigator.js
+++ MDSNavigator/MDSNavigator.js
@@ -5,6 +5,9 @@ class MDSNavigator {
   APIRootURL = 'https://mds.canalyst.com/api/';
 
   constructor(token) {
+    if (!token)
+      throw new Error('Must pass API token as parameter in constructor.')
+
     this.token = token;
 
     this.instance = axios.create({
@@ -18,11 +21,32 @@ class MDSNavigator {
 
   async initCompanyList(options) {
     this.companyListArray = await new MDSCompanyList(this.token).getCompanyList(options);
+
+    return this.companyListArray;
   }
 
   getCompanyListArray() {
     return this.companyListArray;
   }
+
+  // async getHistoricalData(ticker) {
+  //   options = options || {};
+  //   const { csin, ticker } = options;
+
+  //   if (csin === undefined && ticker === undefined) 
+  //     throw new Error('Must include csin or ticker in parameters.');
+
+  //   if ( ticker.ticker === undefined || ticker.tickerType === undefined )
+  //     throw new Error('ticker.ticker and ticker.tickerType must be defined.'); 
+  // }
+
+  // async getForecastData(options) {
+
+  // }
+
+  // async getEquityModelSeriesSet(options) {
+
+  // }
 }
 
 module.exports = MDSNavigator;
