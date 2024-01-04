6d754c1b6e2044b8d5feeb46041e604790c10bd2
Holland Ninow <holland.ninow@gmail.com>
2024-01-03 18:38:39 -0800

    rewrite unit tests for constructor and getCompanyList

--- test/unit-tests/MDSCompanyList.js
+++ test/unit-tests/MDSCompanyList.js
@@ -1,40 +1,72 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
+const proxyquire = require('proxyquire');
+const fs = require('fs');
 const MDSCompanyList = require('../../MDSNavigator/MDSCompanyList');
+const { convertCSVToArray } = require('../../utils/convertCSVToArray');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const token = process.env.CANALYST_JWT;
 
 describe('MDSCompanyList', () => {
   describe('constructor', () => {
-    it('should initialize when passed any token', () => {
-      const token = 'test'
-      const list = new MDSCompanyList(token);
-
-      expect(list.instance).to.be.not.undefined;
-      expect(list.instance.defaults.headers.Authorization).to.be.equal(`Bearer ${token}`);
-      expect(list.instance.defaults.baseURL).to.be.equal('https://mds.canalyst.com/api/');
-      expect(list.instance.defaults.timeout).to.be.equal(20000);
-      expect(list.APIQueryURL).to.be.equal('companies/');
-    });
-
-    it('throws an error when not passed a token', () => {
-        try {
-          new MDSCompanyList();
-        } catch (err) {
-          expect(err).to.be.an('Error');
-          expect(err.message).to.be.equal('Must pass API token as parameter in constructor.');
-        }
+    describe('constructor', () => {
+      it('should initialize when passed any token', () => {
+        const token = 'test'
+        const list = new MDSCompanyList(token);
+  
+        expect(list.instance).to.be.not.undefined;
+        expect(list.instance.defaults.headers.Authorization).to.be.equal(`Bearer ${token}`);
+        expect(list.instance.defaults.baseURL).to.be.equal('https://mds.canalyst.com/api/');
+        expect(list.instance.defaults.timeout).to.be.equal(20000);
+        expect(list.APIQueryURL).to.be.equal('companies/');
+      });
+  
+      it('throws an error when not passed a token', () => {
+          try {
+            new MDSCompanyList();
+          } catch (err) {
+            expect(err).to.be.an('Error');
+            expect(err.message).to.be.equal('Must pass API token as parameter in constructor.');
+          }
+      });
     });
   });
 
   describe('getCompanyList', () => {
-    it('should retrieve data when given no parameters', async () => {
-      const list = new MDSCompanyList('test');
-      const listStub = sinon.stub(list, 'getCompanyList');
-      const responseData = { data: 'csv' };
-      listStub.resolves(responseData);
-  
-      const data = await list.getCompanyList();
-  
-      expect(data).to.be.deep.equal(responseData);
+    let axiosInstance, mdsCompanyList;
+
+    beforeEach(() => {
+      axiosInstance = {
+        get: sinon.stub(),
+      }
+
+      const mockedAxios = {
+        create: () => axiosInstance,
+      }
+
+      const MDSCompanyListProxy = proxyquire('../../MDSNavigator/MDSCompanyList', {
+        axios: mockedAxios,
+      });
+
+      mdsCompanyList = new MDSCompanyListProxy('mockToken');
+    });
+
+    it('should return company list without sector', async () => {
+      const responseData = fs.readFileSync(`${__dirname}/../test-data/companyListReinsurance.csv`, 'utf-8');
+      const csv = convertCSVToArray(responseData);
+
+      axiosInstance.get.resolves({
+        data: responseData,
+      });
+
+      const res = await mdsCompanyList.getCompanyList();
+
+      expect(res).to.deep.equal(csv);
+      expect(axiosInstance.get.calledOnce).to.be.true;
+      expect(axiosInstance.get.firstCall.args[0]).to.equal('companies/?format=csv&');
     });
 
     // TODO: doesn't really test the method, it is just circular
@@ -52,7 +84,6 @@ describe('MDSCompanyList', () => {
     });
 
     it('should throw an error when options.sector is not a string', async () => {
-      
       try {
         await new MDSCompanyList('test').getCompanyList({
           sector: 123,
@@ -61,7 +92,6 @@ describe('MDSCompanyList', () => {
         expect(err).to.be.an('Error');
         expect(err.message).to.be.equal('options.sector must be of type String.');
       }
-
     });
   });
 });
