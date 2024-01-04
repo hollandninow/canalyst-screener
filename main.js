090010c4f9c56b04814592ee2121603b39f6021a
Holland Ninow <holland.ninow@gmail.com>
2024-01-03 18:39:52 -0800

    random changes, not important what

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const MDSNavigator = require('./MDSNavigator/MDSNavigator');
+const MDSCompanyList = require('./MDSNavigator/MDSCompanyList');
+const { convertCSVToArray } = require('./utils/convertCSVToArray');
+const axios = require('axios');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const main = async () => {
   try {
-    const navigator = new MDSNavigator(process.env.CANALYST_JWT);
+    // const navigator = new MDSNavigator(process.env.CANALYST_JWT);
 
     // const list = await navigator.initCompanyList({ sector: 'reinsurance' });
     // console.log(list);
+
+    const list = new MDSCompanyList(process.env.CANALYST_JWT);
+    const data = await list.getCompanyList({
+      sector: 'reinsurance',
+    });
+    console.log(data);
 
     // const data = await navigator.getModelData({ 
     //   ticker: 'AAPL_US',
@@ -28,16 +37,17 @@ const main = async () => {
     //   timeSeriesName: 'MO_RIS_EPS_WAD_Adj'
     //   // timeSeriesName: 'MO_RIS_REV'
     // });
-    // console.log(data.results[0].derived_data);
+    // console.log(data);
 
-    const data = await navigator.getModelLTMDataPoint({
-      ticker: 'FTNT_US',
-      periodType: 'historical',
-      periodString: 'Q3-2023',
-      timeSeriesName: 'MO_RIS_EPS_WAD_Adj'
-      // timeSeriesName: 'MO_RIS_REV'
-    });
-    console.log(data);
+    // const data = await navigator.getModelLTMDataPoint({
+    //   ticker: 'FTNT_US',
+    //   periodType: 'historical',
+    //   periodString: 'Q3-2023',
+    //   timeSeriesName: 'MO_BS_SE'
+    //   // timeSeriesName: 'MO_RIS_EPS_WAD_Adj'
+    //   // timeSeriesName: 'MO_RIS_REV'
+    // });
+    // console.log(data);
 
   } catch (err) {
     console.error(err);
