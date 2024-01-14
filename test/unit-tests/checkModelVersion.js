726898965727852a6b39c6c28ba128db82112061
Holland Ninow <holland.ninow@gmail.com>
2024-01-14 15:54:00 -0800

    add unit tests for error handling

--- test/unit-tests/checkModelVersion.js
+++ test/unit-tests/checkModelVersion.js
@@ -4,16 +4,37 @@ const { expect } = require('chai');
 const { checkModelVersion } = require('../../versionChecker/checkModelVersion');
 
 describe('checkModelVersion', () => {
-  it('should throw an error if either options.csin or options.canalystTicker is not provided', async () => {
-
+  it('should throw an error if neither options.csin nor options.canalystTicker are provided', async () => {
+    try {
+      await checkModelVersion( 'mdsToken', 'apiToken');
+    } catch (err) {
+      expect(err).to.be.an('Error');
+      expect(err.message).to.be.equal('One of options.csin or options.canalystTicker must be defined.');
+    }
   });
 
   it('should throw an error if mdsToken is not provided', async () => {
-
+    try {
+      await checkModelVersion( undefined, 'apiToken', {
+        csin: 'test',
+        canalystTicker: 'test',
+      });
+    } catch (err) {
+      expect(err).to.be.an('Error');
+      expect(err.message).to.be.equal('Must provide MDS token.');
+    }
   });
 
   it('should throw an error if screener API token (apiToken) is not provided', async () => {
-
+    try {
+      await checkModelVersion( 'mdsToken', undefined, {
+        csin: 'test',
+        canalystTicker: 'test',
+      });
+    } catch (err) {
+      expect(err).to.be.an('Error');
+      expect(err.message).to.be.equal('Must provide token for screener API.');
+    }
   });
 
   it('should return true if model version on database is the same as model version on MDS', async () => {
